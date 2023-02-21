cfef57bf755f969e1984b0dc52f4f4fc19cb0341
Sean-Peterson05 <Smpeterson241@gmail.com>
2023-02-21 13:47:18 -0500

    Able to pass form props to api call

--- src/components/quoteForm/Confirmation.js
+++ src/components/quoteForm/Confirmation.js
@@ -3,7 +3,7 @@ import { Button, Container } from 'react-bootstrap';
 import APIService from "../../apiService";
 
 class Confirmation extends Component{
-
+        
     client = new APIService();
 
     back  = (e) => {
@@ -18,21 +18,24 @@ class Confirmation extends Component{
 
     createQuote = (e) => {
         console.log("Creating Quote")
+        console.log(this.props.inputValues)
         this.client.createQuote({
-                                "firstName": "Sean",
-                                "lastName": "Peterson",
-                                "email": "test@test.com",
-                                "Address": "testing",
-                                "age": 30,
-                                "LOB" : "AUTO",
-                                "VIN": "E-CAR"
-                                }) 
+                        "firstName": this.props.inputValues.firstName,
+                        "lastName": this.props.inputValues.lastName,
+                        "email" : this.props.inputValues.email,
+                        "Address" : this.props.inputValues.address,
+                        "age": 30,
+                        "LOB" : "AUTO",
+                        "VIN": "E-CAR",
+                        "Premium" : Math.floor(Math.random() * 200) + 100
+                        }) 
+        console.log("quote created")
+        
     }
 
     render(){
         const {inputValues: { firstName, lastName, email, address, city, state, zip }} = this.props;
-        console.log({inputValues: { firstName, lastName, email, address, city, state, zip }})
-        console.log(firstName)
+        console.log(firstName, lastName, email, address, city, state, zip);
         return(
             <Container>
                 <h1 classname="text-center">Get a Quote</h1>
