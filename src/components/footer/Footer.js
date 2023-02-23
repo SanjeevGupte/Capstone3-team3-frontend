86f765dfb45516210e6f4adec25edd46b5aa024b
Elaine2005 <109104244+Elaine2005@users.noreply.github.com>
2023-02-23 15:24:26 -0500

    footer updates

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,40 +1,41 @@
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import { Form, Button, Col, Container, Row, Image, Card, CardColumns } from 'react-bootstrap';
-import img from './hero_image1.png';
+//import img from './hero_image1.png';
 import { Link } from 'react-router-dom';
+import logo from '../slideshow/capstone_logo.png';
 
 function Footer(props) {
 
     return (
-        <Container>
-            <Container className = "jumbotron-fluid">
-            <Jumbotron className = "bg-primary" style={{ padding: '1rem' }}>
+<footer> 
+            <Container >
+                <Jumbotron className = "bg-primary" style={{ padding: '1rem' , postion: 'absolute', bottom: '0', width: '100%' }} >
+ 
                 <Row>
                     <Col >
-                        <h6 className= "text-white"> ESS Insurance </h6>
-                        <Image className="d-block w-25 h-50 justify-content-md-center"
+                        <h6 > ESS Insurance </h6>
+                        <Image
                             src={logo}
                             alt="logo image" />
                     </Col>
                     <Col>
-                        <h6 className= "text-white"> Get a quote today </h6>
-                        <p><Link className= "text-white" to="/agent">Find an Agent</Link></p>
-                        <p><Link className= "text-white" to="/quote">Get Quote</Link></p>
+                        <h6> Get a quote today </h6>
+                        <p><Link  href= "/agent">Find an Agent</Link></p>
+                        <p><Link href= "/quote">Get Quote</Link></p>
                     </Col>
                     <Col>
-                        <h6 className= "text-white"> Contact Us </h6>
-                        <p className= "text-white"> Email: ESS_CustomerService@ESSInsurance.com</p>
-                        <p className= "text-white"> Phone Number: 1-888-888-8888</p>
+                        <h6> Contact Us </h6>
+                        <p> Email: ESS_CustomerService@ESSInsurance.com</p>
+                        <p > Phone Number: 1-888-888-8888</p>
                     </Col>
                 </Row>
-                </Jumbotron>
-
-            </Container>
-        </Container>
-        
+                </Jumbotron> 
+                        </Container>
+                        </footer>
 
 
     );
 }
 
-export default Slideshow;
+
+export default Footer;
