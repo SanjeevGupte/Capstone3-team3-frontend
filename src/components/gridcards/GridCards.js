157feed88f420b22176c9a0f8e98362207584397
Sean-Peterson05 <Smpeterson241@gmail.com>
2023-02-23 15:27:28 -0500

    agent cards beautfied

--- src/components/gridcards/GridCards.js
+++ src/components/gridcards/GridCards.js
@@ -1,28 +1,35 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { CardColumns } from 'react-bootstrap';
+import css from '../../index.css';
 
 function GridCards({dataFromParent}) {
     console.log(dataFromParent)
     return (
         <div data-testid="gridContainer" className="GridCards container mb-3">
-            <Row xs={1} lg={2} className="g-4" >
+            <Row lg={2}>
                 {dataFromParent.map((_, idx) => (
+                    
                     <Col key={idx}>
-                        <Card border='dark'>
-                            <Card.Img variant="top"
-                            src={dataFromParent[idx].profileImage}
-                            style = {{width:100, height: 150}}/>
-                            <Card.Body>
-                                <Card.Title>{dataFromParent[idx].firstName} {dataFromParent[idx].lastName}</Card.Title>
-                                <Card.Subtitle>{dataFromParent[idx].Title} </Card.Subtitle>
-                                <br></br>
-                                <Card.Text> 
-                                    <p>E-Mail: {dataFromParent[idx].email} </p>
-                                    <p>Phone Number: {dataFromParent[idx].phoneNumber} </p>
-                                    <p>State: {dataFromParent[idx].state}</p>
-                                </Card.Text>
-                            </Card.Body>
+                        <Card border='primary'>
+                            <Col>
+                                    <Card.Img variant="top"
+                                    src={dataFromParent[idx].profileImage}
+                                    style = {{width:100, height: 150, display:'block', margin: 'auto'}}/>
+                            </Col>
+                            <Col>
+                                <Card.Body>
+                                    <Card.Title>{dataFromParent[idx].firstName} {dataFromParent[idx].lastName}</Card.Title>
+                                    <Card.Subtitle>{dataFromParent[idx].Title} </Card.Subtitle>
+                                    <br></br>
+                                    <Card.Text> 
+                                        <p>E-Mail: {dataFromParent[idx].email} </p>
+                                        <p>Phone Number: {dataFromParent[idx].phoneNumber} </p>
+                                        <p>State: {dataFromParent[idx].state}</p>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Col>
                         </Card>
                         <br></br>
                     </Col>
